cd5d97488d196c179e0f4c7a6cd9d562197653c7
Dario Vladovic <d.vladimyr@gmail.com>
2019-07-31 21:29:03 +0200

    Refactor user migration :wrench:

--- server/common/database/migrations/20180403080953-create-user.js
+++ server/common/database/migrations/20180403080953-create-user.js
@@ -1,53 +1,51 @@
 'use strict';
 
-const { role } = require('../../../../common/config');
-const values = require('lodash/values');
+const TABLE_NAME = 'user';
 
-module.exports = {
-  up: (queryInterface, Sequelize) => queryInterface.createTable('user', {
-    id: {
-      type: Sequelize.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-      allowNull: false
-    },
-    email: {
-      type: Sequelize.STRING,
-      allowNull: false,
-      unique: true
-    },
-    password: {
-      type: Sequelize.STRING
-    },
-    role: {
-      type: Sequelize.ENUM(values(role)),
-      allowNull: false
-    },
-    token: {
-      type: Sequelize.STRING(500)
-    },
-    firstName: {
-      type: Sequelize.STRING,
-      field: 'first_name'
-    },
-    lastName: {
-      type: Sequelize.STRING,
-      field: 'last_name'
-    },
-    createdAt: {
-      type: Sequelize.DATE,
-      field: 'created_at',
-      allowNull: false
-    },
-    updatedAt: {
-      type: Sequelize.DATE,
-      field: 'updated_at',
-      allowNull: false
-    },
-    deletedAt: {
-      type: Sequelize.DATE,
-      field: 'deleted_at'
-    }
-  }),
-  down: (queryInterface, Sequelize) => queryInterface.dropTable('user')
-};
+exports.up = (queryInterface, Sequelize) => queryInterface.createTable(TABLE_NAME, {
+  id: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    unique: true
+  },
+  password: {
+    type: Sequelize.STRING
+  },
+  role: {
+    type: Sequelize.ENUM('ADMIN', 'USER'),
+    allowNull: false
+  },
+  token: {
+    type: Sequelize.STRING(500)
+  },
+  firstName: {
+    type: Sequelize.STRING,
+    field: 'first_name'
+  },
+  lastName: {
+    type: Sequelize.STRING,
+    field: 'last_name'
+  },
+  createdAt: {
+    type: Sequelize.DATE,
+    field: 'created_at',
+    allowNull: false
+  },
+  updatedAt: {
+    type: Sequelize.DATE,
+    field: 'updated_at',
+    allowNull: false
+  },
+  deletedAt: {
+    type: Sequelize.DATE,
+    field: 'deleted_at'
+  }
+});
+
+exports.down = queryInterface => queryInterface.dropTable(TABLE_NAME);
