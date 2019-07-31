486c2246c2dda0d72daa444aa7e0f8cd99df651c
Dario Vladovic <d.vladimyr@gmail.com>
2019-08-01 00:28:23 +0200

    Extract filters to common folder :wrench:

--- client/main/index.js
+++ client/main/index.js
@@ -1,11 +1,12 @@
 import App from './App';
 import router from './router';
 import store from './store';
-import truncate from 'truncate';
+import { truncate } from '@/common/filters';
 import VeeValidate from '@/common/validation';
 import Vue from 'vue';
 import VueVisible from 'vue-visible';
 
+Vue.filter('truncate', truncate);
 Vue.use(VeeValidate, {
   delay: 700,
   fieldsBagName: 'vFields',
@@ -13,10 +14,6 @@ Vue.use(VeeValidate, {
   inject: false
 });
 Vue.use(VueVisible);
-
-Vue.filter('truncate', (value, maxLength = 50) => {
-  return value && truncate(value, maxLength);
-});
 
 // eslint-disable-next-line no-new
 new Vue({
