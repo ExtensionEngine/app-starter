a644467698850061a850635542e1e282d1a8716a
Miro Dojkic <mdojkic@gmail.com>
2021-04-16 11:23:55 +0200

    Add missing controller types 🏷️

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -1,21 +1,22 @@
 import User from 'App/Models/User';
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
 
 export default class UsersController {
   public async index() {
     return User.all();
   }
 
-  public async store(ctx) {
+  public async store(ctx: HttpContextContract) {
     const data = ctx.request.only(['fullName']);
     return User.create(data);
   }
 
-  public async show(ctx) {
+  public async show(ctx: HttpContextContract) {
     const id = ctx.request.param('id');
     return User.findOrFail(id);
   }
 
-  public async update(ctx) {
+  public async update(ctx: HttpContextContract) {
     const id = ctx.request.param('id');
     const { fullName } = ctx.request.only(['fullName']);
     const user = await User.findOrFail(id);
@@ -23,7 +24,7 @@ export default class UsersController {
     return user.save();
   }
 
-  public async destroy(ctx) {
+  public async destroy(ctx: HttpContextContract) {
     const id = ctx.request.param('id');
     const user = await User.findOrFail(id);
     return user.delete();
