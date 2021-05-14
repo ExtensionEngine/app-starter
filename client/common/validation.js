2f31eeeda2e2d0db2b35ec93cfb74366ac209952
Luka Saric <rpg.ari7@gmail.com>
2021-05-14 10:21:45 +0200

    Validation rules tree shaking :rocket:

--- client/common/validation.js
+++ client/common/validation.js
@@ -1,28 +1,37 @@
-import * as rules from 'vee-validate/dist/rules';
+import { alpha, email, is, max, mimes, min, required } from 'vee-validate/dist/rules';
 import { extend } from 'vee-validate';
 import forEach from 'lodash/forEach';
+import { messages } from 'vee-validate/dist/locale/en.json';
+import snakeCase from 'lodash/snakeCase';
 import userApi from '@/admin/api/user';
 
 const alphanumerical = {
   validate: value => (/\d/.test(value) && /[a-zA-Z]/.test(value)),
-  message: 'The {_field_} field must contain at least 1 letter and 1 numeric value'
+  message: 'The {_field_} field must contain at least 1 letter and 1 numeric value.'
 };
 
 const uniqueEmail = {
   params: ['userData'],
   validate: (email, { userData }) => {
     if (userData && email === userData.email) return true;
-    return userApi.fetch({ filter: { email } }).then(({ total }) => !total);
+    return userApi.fetch({ params: { email } }).then(({ total }) => !total);
   },
-  message: 'The {_field_} is not unique'
+  message: 'The {_field_} is not unique.'
 };
 
-const configuredRules = {
-  ...rules,
+const rules = {
+  alpha,
   alphanumerical,
-  unique_email: uniqueEmail,
-  max_value: { ...rules.max_value, message: 'The {_field_} must be {max} or less' },
-  min_value: { ...rules.min_value, message: 'The {_field_} must be {min} or more' }
+  email,
+  is,
+  max,
+  min,
+  mimes,
+  required,
+  uniqueEmail
 };
 
-forEach(configuredRules, (rule, name) => extend(name, { ...rule }));
+forEach(rules, (rule, name) => extend(snakeCase(name), {
+  message: messages[name],
+  ...rule
+}));
