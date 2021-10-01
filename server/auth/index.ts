ef6479078c723acf3c872e7461d812519cab0311
Luka Saric <rpg.ari7@gmail.com>
2021-10-01 12:33:36 +0200

    Add req limiter

--- server/auth/index.ts
+++ server/auth/index.ts
@@ -2,6 +2,7 @@ import App, { Router } from 'express';
 import { authenticate } from './middleware';
 import Controller from './controller';
 import { IContainer } from 'bottlejs';
+import { requestLimiter } from '../middleware/request';
 import Service from './service';
 
 export default {
@@ -14,5 +15,10 @@ function createRouter({ authController }: IContainer): Router {
   return App.Router()
     .post('/login', authenticate('local'), authController.me)
     .post('/forgot-password', authController.forgotPassword)
-    .post('/reset-password', authenticate('token'), authController.resetPassword);
+    .post(
+      '/reset-password',
+      requestLimiter(),
+      authenticate('token'),
+      authController.resetPassword
+    );
 }
