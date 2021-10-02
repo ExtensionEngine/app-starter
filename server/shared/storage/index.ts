b9801309f4ca3b0fb7c1b1c289c2aa38d1dbcb84
Luka Saric <rpg.ari7@gmail.com>
2021-10-02 18:58:13 +0200

    Replace void with any on storage wrapper methods

--- server/shared/storage/index.ts
+++ server/shared/storage/index.ts
@@ -10,47 +10,47 @@ class Storage {
     autobind(this);
   }
 
-  getFile(key: string, options = {}): Promise<void> {
+  getFile(key: string, options = {}): Promise<any> {
     return this.#provider.getFile(key, options);
   }
 
-  createReadStream(key: string, options = {}): Promise<void> {
+  createReadStream(key: string, options = {}): Promise<any> {
     return this.#provider.createReadStream(key, options);
   }
 
-  saveFile(key: string, data = {}, options = {}): Promise<void> {
+  saveFile(key: string, data = {}, options = {}): Promise<any> {
     return this.#provider.saveFile(key, data, options);
   }
 
-  createWriteStream(key: string, options = {}): Promise<void> {
+  createWriteStream(key: string, options = {}): Promise<any> {
     return this.#provider.createWriteStream(key, options);
   }
 
-  deleteFile(key: string, options = {}): Promise<void> {
+  deleteFile(key: string, options = {}): Promise<any> {
     return this.#provider.deleteFile(key, options);
   }
 
-  deleteFiles(keys: string[], options = {}): Promise<void> {
+  deleteFiles(keys: string[], options = {}): Promise<any> {
     return this.#provider.deleteFiles(keys, options);
   }
 
-  listFiles(options = {}): Promise<void> {
+  listFiles(options = {}): Promise<any> {
     return this.#provider.listFiles(options);
   }
 
-  fileExists(key: string, options = {}): Promise<void> {
+  fileExists(key: string, options = {}): Promise<any> {
     return this.#provider.fileExists(key, options);
   }
 
-  getFileUrl(key: string, options = {}): Promise<void> {
+  getFileUrl(key: string, options = {}): Promise<any> {
     return this.#provider.getFileUrl(key, options);
   }
 
-  moveFile(key: string, newKey: string, options = {}): Promise<void> {
+  moveFile(key: string, newKey: string, options = {}): Promise<any> {
     return this.#provider.moveFile(key, newKey, options);
   }
 
-  copyFile(key: string, newKey: string, options = {}): Promise<void> {
+  copyFile(key: string, newKey: string, options = {}): Promise<any> {
     return this.#provider.copyFile(key, newKey, options);
   }
 }
