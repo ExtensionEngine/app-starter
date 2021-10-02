3af424d6dd28e0f61bb56bc67aea6bf98e9df5e2
Luka Saric <rpg.ari7@gmail.com>
2021-10-02 16:35:51 +0200

    authorize cleanup

--- server/auth/middleware/authorize.ts
+++ server/auth/middleware/authorize.ts
@@ -6,8 +6,8 @@ function authorize(allowed: string[]): RequestHandler {
   allowed.push(roles.ADMIN);
   return ({ user }: Request, _res: Response, next: NextFunction) => {
     if (!user) throw new Unauthorized('Access restricted');
-    if (allowed.includes(user.role)) return next();
-    throw new Forbidden('Access denied');
+    if (!allowed.includes(user.role)) throw new Forbidden('Access denied');
+    return next();
   };
 }
 
