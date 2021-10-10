0df1426bf5ded9b3d8b2105f96d6abe500488b9d
Luka Saric <rpg.ari7@gmail.com>
2021-10-10 19:03:17 +0200

    Renamed paranoid to archived :eyes:

--- server/middleware/pagination.ts
+++ server/middleware/pagination.ts
@@ -23,8 +23,8 @@ class ParsePaginationMiddleware implements IMiddleware {
 
   async handle(req: Request, _: Response, next: NextFunction): Promise<void> {
     const { limit, offset, sortBy = 'createdAt', sortOrder = 'ASC' } = req.query;
-    const paranoidKeywords = ['archived', 'deleted', 'destroyed'];
-    const showArchived = paranoidKeywords.some(it => yn(req.query[it]));
+    const archivedKeywords = ['archived', 'deleted', 'destroyed'];
+    const showArchived = archivedKeywords.some(it => yn(req.query[it]));
     req.pagination = {
       limit: Number(limit) || 100,
       offset: Number(offset) || 0,
