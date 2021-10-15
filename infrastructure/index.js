41d462bd24d98666e62ee4b10d7c6683d5fa8bed
Ivo Kovacevic <ikovacevic96k@gmail.com>
2021-10-15 17:33:36 +0200

    Add ecr repo name to the context 👷

--- infrastructure/index.js
+++ infrastructure/index.js
@@ -2,10 +2,10 @@
 
 // const eks = require('./eks');
 // const rds = require('./rds');
-const s3 = require('./s3');
+// const s3 = require('./s3');
 // const vpc = require('./vpc');
 
 module.exports = {
   // dbHost: rds.address,
-  siteBucketEndpoint: s3.siteBucket.websiteEndpoint
+  // siteBucketEndpoint: s3.siteBucket.websiteEndpoint
 };
