473cd7b02a5512d15a7464eaa7b12af542b2d701
Luka Saric <rpg.ari7@gmail.com>
2021-10-21 14:23:00 +0200

    Move seeding admin within the users module

--- server/shared/database/seeds/index.ts
+++ server/shared/database/seeds/index.ts
@@ -1,8 +1,6 @@
 import { EntityManager } from '@mikro-orm/postgresql';
-import seedAdmin from './admin';
 import seedUsers from './users';
 
 export default async (em: EntityManager): Promise<void> => {
-  await seedAdmin(em);
   await seedUsers(em);
 };
--- server/shared/database/seeds/users.ts
+++ server/shared/database/seeds/users.ts
@@ -4,11 +4,12 @@ import times from 'lodash/times';
 import User from '../../../user/model';
 
 async function seedUsers(em: EntityManager): Promise<void> {
+  const admin = new User('Admin', 'Example', 'admin@example.org', roles.ADMIN);
   const users = times(10, idx => {
     const suffix = idx || '';
     return new User(`User ${suffix}`, 'Example', `user${suffix}@example.org`, roles.USER);
   });
-  await em.persistAndFlush(users);
+  await em.persistAndFlush([admin, ...users]);
 }
 
 export default seedUsers;
