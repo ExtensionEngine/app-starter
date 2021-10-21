31b5ead4ebf942a41a84f6035ad2c76883f1e348
Ivo Kovacevic <ikovacevic96k@gmail.com>
2021-10-21 13:36:41 +0200

    Inject envs into cypress env config ✨

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,23 +1,26 @@
 'use strict';
-/// <reference types="cypress" />
-// ***********************************************************
-// This example plugins/index.js can be used to load plugins
-//
-// You can change the location of this file or turn off loading
-// the plugins file with the 'pluginsFile' configuration option.
-//
-// You can read more here:
-// https://on.cypress.io/plugins-guide
-// ***********************************************************
 
-// This function is called when a project is opened or re-opened (e.g. due to
-// the project's config changing)
+require('dotenv').config({ path: require.resolve('../.env') });
+const joi = require('joi');
+
+const ENV_SECRETS = [
+  'API_URL'
+];
+
+console.log('process.env: ', process.env.API_URL);
+const schema = joi.object({
+  API_URL: joi.string().required()
+}).unknown();
+joi.assert(process.env, schema);
 
 /**
  * @type {Cypress.PluginConfig}
  */
-// eslint-disable-next-line no-unused-vars
-module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
+module.exports = async (on, config) => {
+  config.baseUrl = process.env.API_URL;
+  console.log('config.baseUrl: ', config.baseUrl);
+  ENV_SECRETS.forEach(secret => {
+    config.env[secret] = process.env[secret];
+  });
+  return config;
 };
