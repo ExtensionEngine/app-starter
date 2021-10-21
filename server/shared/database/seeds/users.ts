3d891bacf7773fa5b430c03cebf93956a03c5505
Luka Saric <rpg.ari7@gmail.com>
2021-10-21 16:01:15 +0200

    Add missing pw within the user seed

--- server/shared/database/seeds/users.ts
+++ server/shared/database/seeds/users.ts
@@ -4,10 +4,22 @@ import times from 'lodash/times';
 import User from '../../../user/model';
 
 async function seedUsers(em: EntityManager): Promise<void> {
-  const admin = new User('Admin', 'Example', 'admin@example.org', roles.ADMIN);
+  const admin = new User(
+    'Admin',
+    'Example',
+    'admin@example.org',
+    roles.ADMIN,
+    'admin123'
+  );
   const users = times(10, idx => {
     const suffix = idx || '';
-    return new User(`User ${suffix}`, 'Example', `user${suffix}@example.org`, roles.USER);
+    return new User(
+      `User ${suffix}`,
+      'Example',
+      `user${suffix}@example.org`,
+      roles.USER,
+      'user123'
+    );
   });
   await em.persistAndFlush([admin, ...users]);
 }
